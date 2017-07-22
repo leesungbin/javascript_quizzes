62d853ce79da56002eea3c1ab616b54f5e2a7c29
LSB <lsbskate@naver.com>
2017-07-22 13:12:26 +0900

    huh?

--- q1_modules/foo.js
+++ q1_modules/foo.js
@@ -1,2 +1,15 @@
 const fooString='FOO';
 export default fooString;
+
+
+//default export
+/*
+export const fooString ='FOO';
+=>
+import {fooString } from './foo';
+
+export default 'BAR';
+=>
+import 아무거나 from './bar';
+import { default as 아무거나 } from './bar';
+*/
